f182c33cf169816d9aae74a69e873a1e6fe176bc
yulangren <1368419640@qq.com>
2018-11-18 16:15:59 +0800

    项目的应用创建

--- super/super/settings.py
+++ super/super/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'super.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'       # 设置为中文
 
 TIME_ZONE = 'UTC'
 
